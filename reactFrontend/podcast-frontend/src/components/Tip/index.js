1d455ae0ab897e167000429a79f43df54476eed8
Nash <panashemhondeh@gmail.com>
2023-03-19 01:18:53 +0200

    responsive design to contact page

--- reactFrontend/podcast-frontend/src/components/Tip/index.js
+++ reactFrontend/podcast-frontend/src/components/Tip/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import "./index.scss"
+import React, { useEffect, useRef, useState } from "react";
+import "./index.scss";
 
-const Tip = ({tip}) => {
+const Tip = ({ tip }) => {
+  const tipRef = useRef();
+  const [display, setDisplay] = useState("initial");
+
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setDisplay("none");
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+  useOutsideAlerter(tipRef)
   return (
-    <div id="tip"><p>
-        {tip}</p></div>
-  )
-}
 
-export default Tip
+    <div id="tip" ref={tipRef} style={{display: display}}>
+      <p>Tip: {tip}</p>
+    </div>
+  );
+};
+
+export default Tip;
